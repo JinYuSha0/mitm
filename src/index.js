47b2164da38040c72fcd492cf947cd528e091405
邵瑾瑜 <1009943858@qq.com>
2018-11-05 01:37:41 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
-const createFakeCertificate = require('./utils/createFakeCertificate')
+const http = require('http')
+const url = require('url')
+const net = require('net')
+const createFakeHttpsWebSite = require('./utils/createFakeHttpsWebSite')
 
-async function main () {
-	const certificate = await createFakeCertificate('tmall.com')
-	console.log(certificate)
-}
+let httpTunnel = new http.Server()
+let port = 8080
 
-main()
+httpTunnel.listen(port, () => {
+    console.log(`HTTPS中间人代理启动成功，端口${port}`)
+})
+
+httpTunnel.on('error', (e) => {
+    if (e.code == 'EADDRINUSE') {
+        console.error(`端口：${port}，已被占用`);
+    } else {
+        console.error(e)
+    }
+})
+
+httpTunnel.on('connect', (req, cltSocket, head) => {
+    const srvUrl = url.parse(`http://${req.url}`)
+    createFakeHttpsWebSite(srvUrl.hostname, (port) => {
+        const srvSocket = net.connect(port, '127.0.0.1', () => {
+            cltSocket.write('HTTP/1.1 200 Connection Established\r\n' +
+                'Proxy-agent: MITM-proxy\r\n' +
+                '\r\n')
+            srvSocket.write(head)
+            srvSocket.pipe(cltSocket)
+            cltSocket.pipe(srvSocket)
+        })
+        srvSocket.on('error', (e) => {
+            console.error(e)
+        })
+    })
+})
--- src/utils/createFakeCertificate.js
+++ src/utils/createFakeCertificate.js
@@ -1,14 +1,207 @@
 const fs = require('fs')
-const getPerrCertificate = require('./getPerrCertificate')
+const path = require('path')
+const forge = require('node-forge')
+const https = require('https')
 
-async function createFakeCertificate (host) {
+const pki = forge.pki
+const sslDir = path.resolve('../ssl')
+const caCertPath = path.join(sslDir, '/rootCA.crt')
+const caKeyPath = path.join(sslDir, '/rootCA.key.pem')
+
+function delay (ms, error) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (error) {
+                reject(error)
+            } else {
+                resolve()
+            }
+        }, ms)
+    })
+}
+
+// 获取https证书信息
+function getCertificateInfo (host) {
+    const timeoutPromise = delay(500, new Error('获取https证书信息超时'))
+    const requestPromise = new Promise((resolve, reject) => {
+        const req = https.request({
+            host,
+            port: 443,
+            method: 'GET'
+        }, res => {
+            resolve(res.connection.getPeerCertificate())
+        })
+
+        req.on('error', err => {
+            reject(new Error('获取证书信息失败!'))
+        })
+
+        req.end()
+    })
+
+    return Promise.race([requestPromise, timeoutPromise])
+}
+
+// 生成CA根证书
+function createFakeCaCertificate() {
+    const keys = pki.rsa.generateKeyPair(1024)
+    const cert = pki.createCertificate()
+    cert.publicKey = keys.publicKey
+
+    // 设置有效时间
+    cert.validity.notBefore = new Date()
+    cert.validity.notAfter = new Date()
+    cert.validity.notBefore.setFullYear(cert.validity.notBefore.getFullYear())
+    cert.validity.notAfter.setFullYear(cert.validity.notAfter.getFullYear() + 1)
+
+    const attrs = [
+        {
+            name: 'commonName',
+            value: 'sjy'
+        }, {
+            name: 'countryName',
+            value: 'CN'
+        }, {
+            shortName: 'ST',
+            value: 'GuangDong'
+        }, {
+            name: 'localityName',
+            value: 'ShenZhen'
+        }, {
+            name: 'organizationName',
+            value: 'sjy'
+        }, {
+            shortName: 'OU',
+            value: 'sjy'
+        }
+    ]
+
+    cert.setSubject(attrs)
+    cert.setIssuer(attrs)
+    cert.setExtensions([{
+        name: 'basicConstraints',
+        critical: true,
+        cA: true
+    }, {
+        name: 'keyUsage',
+        critical: true,
+        keyCertSign: true
+    }, {
+        name: 'subjectKeyIdentifier'
+    }])
+
+    // 用自己的私钥签名
+    cert.sign(keys.privateKey, forge.md.sha256.create())
+
+    const certPem = pki.certificateToPem(cert)
+    const keyPem = pki.privateKeyToPem(keys.privateKey)
+
+    fs.writeFileSync(caCertPath, certPem)
+    fs.writeFileSync(caKeyPath, keyPem)
+}
+
+// 生成子证书
+async function createFakeCertificate (domain) {
 	try {
-		const certInfo = await getPerrCertificate(host)
-		const CN = certInfo.subject.CN
-		const subjectaltname = certInfo.subjectaltname
-		return ''
+        const certPath = path.join(sslDir, `/other/cert_${domain}.crt`)
+        const keyPath = path.join(sslDir, `/other/key_${domain}.pem`)
+
+        if (fs.existsSync(certPath) && fs.existsSync(keyPath)) {
+            const keyPem = fs.readFileSync(keyPath).toString()
+            const certPem = fs.readFileSync(certPath).toString()
+            return {
+                key: pki.privateKeyFromPem(keyPem),
+                cert: pki.certificateFromPem(certPem),
+                keyPem,
+                certPath,
+            }
+        }
+
+        if (fs.existsSync(caCertPath) && fs.existsSync(caKeyPath)) {
+	        // 用ca根证书生成子证书
+	        const caCertPem = fs.readFileSync(caCertPath)
+            const caKeyPem = fs.readFileSync(caKeyPath)
+            const caCert = pki.certificateFromPem(caCertPem)
+            const caKey = pki.privateKeyFromPem(caKeyPem)
+
+            const keys = pki.rsa.generateKeyPair(2048)
+            const cert = pki.createCertificate()
+            cert.publicKey = keys.publicKey
+            cert.serialNumber = '01'
+
+            // 设置有效时间
+            cert.validity.notBefore = new Date()
+            cert.validity.notAfter = new Date()
+            cert.validity.notBefore.setFullYear(cert.validity.notBefore.getFullYear())
+            cert.validity.notAfter.setFullYear(cert.validity.notAfter.getFullYear() + 1)
+
+            const { subject, issuer } = await getCertificateInfo(domain)
+
+            // 设置发行者为根证书
+            cert.setIssuer(caCert.subject.attributes)
+            // 设置提供给站点的信息
+            cert.setSubject(subject)
+            cert.setExtensions([{
+                name: 'basicConstraints',
+                critical: true,
+                cA: false
+            },
+                {
+                    name: 'keyUsage',
+                    critical: true,
+                    digitalSignature: true,
+                    contentCommitment: true,
+                    keyEncipherment: true,
+                    dataEncipherment: true,
+                    keyAgreement: true,
+                    keyCertSign: true,
+                    cRLSign: true,
+                    encipherOnly: true,
+                    decipherOnly: true
+                },
+                {
+                    name: 'subjectAltName',
+                    altNames: [{
+                        type: 2,
+                        value: domain
+                    }]
+                },
+                {
+                    name: 'subjectKeyIdentifier'
+                },
+                {
+                    name: 'extKeyUsage',
+                    serverAuth: true,
+                    clientAuth: true,
+                    codeSigning: true,
+                    emailProtection: true,
+                    timeStamping: true
+                },
+                {
+                    name:'authorityKeyIdentifier'
+                }])
+
+            // 用CA根证书私钥签名 生成子证书
+            cert.sign(caKey, forge.md.sha256.create())
+
+            const certPem = pki.certificateToPem(cert)
+            const keyPem = pki.privateKeyToPem(keys.privateKey)
+            fs.writeFileSync(certPath, certPem)
+            fs.writeFileSync(keyPath, keyPem)
+
+            return {
+	            cert,
+                key: keys.privateKey,
+                certPem,
+                keyPem
+            }
+        } else {
+            createFakeCaCertificate()
+            return createFakeCertificate(domain)
+        }
 	} catch (err) {
-		throw err
+		console.error(`${domain}: 生成证书失败,原因:${err.message}`)
+        throw err
 	}
 }
 
