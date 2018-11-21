d8cfa139ca7bee30bbf863b4b7179c8616c09942
o-jingyu.shao <o-jingyu.shao@dji.com>
2018-11-21 12:25:05 +0800

    新增deflate解码方式

--- src/utils/decode.js
+++ src/utils/decode.js
@@ -4,6 +4,7 @@ const iltorb = require('iltorb')
 const decode = {
   gzip: (body) => {
     return new Promise((resolve, reject) => {
+      if (!body) reject('body is null')
       zlib.gunzip(body, (err, dezipped) => {
         if (err || dezipped == null) {
           reject(err || new Error('dezipped is null'))
@@ -15,10 +16,23 @@ const decode = {
   },
   brotli: (body) => {
     return new Promise((resolve, reject) => {
+      if (!body) reject('body is null')
       iltorb.decompress(body).then(value => {
         resolve(value.toString())
       }).catch(err => {
         reject(err)
+      })
+    })
+  },
+  deflate: (body) => {
+    return new Promise((resolve, reject) => {
+      if (!body) reject('body is null')
+      zlib.inflate(body, (err, dedeflate) => {
+        if (err || dedeflate == null) {
+          reject(err || new Error('dedeflate is null'))
+          return
+        }
+        resolve(dedeflate)
       })
     })
   }
@@ -32,7 +46,7 @@ module.exports = async (encode, body) => {
     case 'br':
       return (await decode.brotli(body))
     case 'deflate':
-      return body
+      return (await decode.deflate(body))
     default:
       return body
   }
