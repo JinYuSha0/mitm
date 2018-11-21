203047e9503db619e7477b0f3968bf715b13ae51
o-jingyu.shao <o-jingyu.shao@dji.com>
2018-11-21 15:39:51 +0800

    修复post请求传参问题

--- src/utils/requestHandle.js
+++ src/utils/requestHandle.js
@@ -8,7 +8,7 @@ const injectScript = require('./injectScript')
 
 const randomPath = Math.random().toString(36).split('.')[1]
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   try {
     if (req.headers.host === 'www.cert.com') {
       // 定义一个域名下载根证书便于安装
@@ -29,45 +29,55 @@ module.exports = (req, res) => {
       }
     } else if (req.method.toLowerCase() === 'get' && req.headers.accept.match('text/html')) {
       // 区别处理html请求 用于注入script
-      request(getRequestOptions(req, res), async (error, response, body) => {
-        if (error) {
-          res.end()
-        }
-        // 判断返回头中的content-encoding 选择解码方式
-        const encode = response.headers['content-encoding']
-        if (encode) {
-          try {
-            body = await decode(encode, body)
-          } catch (err) {
-            res.writeHead(500)
-            res.end('Decode Error')
-            throw err
+      try {
+        const options = await getRequestOptions(req, res)
+        request(options, async (error, response, body) => {
+          if (error) {
+            res.end()
           }
-        }
-        if (response) {
-          // 为什么要去这两个头
-          // 1.content-encoding：因为代理已经解码过了 不需要浏览器再次解码
-          // 2.content-length：因为解压前的长度比解压后的长度小，会导致原内容丢失
-          // 浅拷贝 response.headers
-          const headers = Object.assign({}, response.headers)
-          delete headers['content-encoding']
-          delete headers['content-length']
-          res.writeHead(response.statusCode, headers)
-          if (body) {
-            body = injectScript(body, `/${randomPath}/inject.js`)
-            res.end(body)
-          } else {
-            res.end('No Response Body')
+          // 判断返回头中的content-encoding 选择解码方式
+          const encode = response.headers['content-encoding']
+          if (encode) {
+            try {
+              body = await decode(encode, body)
+            } catch (err) {
+              res.writeHead(500)
+              res.end('Decode Error')
+              throw err
+            }
           }
-        }
-      })
+          if (response) {
+            // 为什么要去这两个头
+            // 1.content-encoding：因为代理已经解码过了 不需要浏览器再次解码
+            // 2.content-length：因为解压前的长度比解压后的长度小，会导致原内容丢失
+            // 浅拷贝 response.headers
+            const headers = Object.assign({}, response.headers)
+            delete headers['content-encoding']
+            delete headers['content-length']
+            res.writeHead(response.statusCode, headers)
+            if (body) {
+              body = injectScript(body, `/${randomPath}/inject.js`)
+              res.end(body)
+            } else {
+              res.end('No Response Body')
+            }
+          }
+        })
+      } catch (err) {
+        throw err
+      }
     } else {
       // 其余请求走字节流的形式
-      request(getRequestOptions(req, res), (err, response, body) => {
-        if (err) {
-          res.end()
-        }
-      }).pipe(res)
+      try {
+        const options = await getRequestOptions(req, res)
+        request(options, (err, response, body) => {
+          if (err) {
+            res.end()
+          }
+        }).pipe(res)
+      } catch (err) {
+        throw err
+      }
     }
   } catch (e) {
     res.end()
